551f63783672024c480af202e7d8b5d67c57022d
koral044 <152703749+koral044@users.noreply.github.com>
2023-12-05 01:47:46 +0400

    Add files via upload

---
+++ ProjectAC/zonecpp-main/project/cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Emin/Desktop/zonecpp-main/project/heapsort.cpp" "CMakeFiles/project.dir/heapsort.cpp.obj" "gcc" "CMakeFiles/project.dir/heapsort.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/insertion.cpp" "CMakeFiles/project.dir/insertion.cpp.obj" "gcc" "CMakeFiles/project.dir/insertion.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/main.cpp" "CMakeFiles/project.dir/main.cpp.obj" "gcc" "CMakeFiles/project.dir/main.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/quicksort.cpp" "CMakeFiles/project.dir/quicksort.cpp.obj" "gcc" "CMakeFiles/project.dir/quicksort.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/radixsort.cpp" "CMakeFiles/project.dir/radixsort.cpp.obj" "gcc" "CMakeFiles/project.dir/radixsort.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/randomarray.cpp" "CMakeFiles/project.dir/randomarray.cpp.obj" "gcc" "CMakeFiles/project.dir/randomarray.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/test.cpp" "CMakeFiles/project.dir/test.cpp.obj" "gcc" "CMakeFiles/project.dir/test.cpp.obj.d"
+  "C:/Users/Emin/Desktop/zonecpp-main/project/timemeasurement.cpp" "CMakeFiles/project.dir/timemeasurement.cpp.obj" "gcc" "CMakeFiles/project.dir/timemeasurement.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ProjectAC/zonecpp-main/project/cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/heapsort.cpp.obj"
+  "CMakeFiles/project.dir/heapsort.cpp.obj.d"
+  "CMakeFiles/project.dir/insertion.cpp.obj"
+  "CMakeFiles/project.dir/insertion.cpp.obj.d"
+  "CMakeFiles/project.dir/main.cpp.obj"
+  "CMakeFiles/project.dir/main.cpp.obj.d"
+  "CMakeFiles/project.dir/quicksort.cpp.obj"
+  "CMakeFiles/project.dir/quicksort.cpp.obj.d"
+  "CMakeFiles/project.dir/radixsort.cpp.obj"
+  "CMakeFiles/project.dir/radixsort.cpp.obj.d"
+  "CMakeFiles/project.dir/randomarray.cpp.obj"
+  "CMakeFiles/project.dir/randomarray.cpp.obj.d"
+  "CMakeFiles/project.dir/test.cpp.obj"
+  "CMakeFiles/project.dir/test.cpp.obj.d"
+  "CMakeFiles/project.dir/timemeasurement.cpp.obj"
+  "CMakeFiles/project.dir/timemeasurement.cpp.obj.d"
+  "libproject.dll.a"
+  "project.exe"
+  "project.exe.manifest"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
